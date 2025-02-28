d86cb9cc0e0c5d10f6d1e5f1ca2bc24c04df7e8c
Thomas W <126Twalsh@frhsd.com>
2025-02-28 12:53:27 -0500

    Day 3

--- js/game.js
+++ js/game.js
@@ -1,24 +1,26 @@
-let lastRenderTime = 0;
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } 
 from "./snake.js";
 
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById
+('game-board')
+
+
 function main(currentTime) {
-    window.requestAnimationFrame(main)
+    window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
     
     
     console.log ('Render');
     lastRenderTime = currentTime;
-    
-
-    lastRenderTime = currentTime;
 
     update();
     draw();
 }
 
-window.requestAnimationFrame
+window.requestAnimationFrame(main);
 
 function update() {
     updateSnake()
@@ -26,5 +28,6 @@ function update() {
 }
 
 function draw() {
-    drawSnake()
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard)
 }
